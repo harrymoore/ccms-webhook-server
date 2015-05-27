e8542d918124e60795203ee7a563e2160d01bcbe
harrymoore <harry.moore@rothburysoftware.com>
2015-05-26 20:21:20 -0400

    add always fail, always succeed and random fail routes

--- server.js
+++ server.js
@@ -1,38 +1,40 @@
-// BASE SETUP
-// =============================================================================
-
-// call the packages we need
+// server.js
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
 
-// ROUTES FOR OUR API
-// =============================================================================
 var router = express.Router();              // get an instance of the express Router
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {                        
-    console.log('req ' + req.ref);
-    res.status(200).json({ message: 'success' });   
+  console.log('req ' + req.ref);
+  res.status(200).json({ message: 'success' });   
 });
 router.get('/taskValidation', function(req, res) {
-    res.status(200).json({ message: 'success' });   
+  // do something useful here. until then always succeed
+  res.status(200).json({ message: 'success' });   
+});
+router.get('/taskValidationRandomFail', function(req, res) {
+  // randomly fail 50% of the time
+  var n = Math.floor(Math.random()*100) < 50;
+  if(Math.floor(Math.random()*10) > 5) {
+    res.status(400).json({ message: 'fail' });   
+  } else {
+    res.status(200).json({ message: 'success'});   
+  }
+});
+router.get('/taskValidationAlwaysSucceed', function(req, res) {
+  res.status(200).json({ message: 'success' });   
+});
+router.get('/taskValidationAlwaysFail', function(req, res) {
+  res.status(400).json({ message: 'fail' });   
 });
 
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
 app.use('/api', router);
 
 // START THE SERVER
-// =============================================================================
 app.listen(port);
 console.log('Running. Listen port: ' + port);
