7c2788e370a5929d19ec3df02b222a7bd12fe7fa
Harry Moore <harrymoore@yahoo.com>
2017-04-18 15:14:17 -0400

    added touch and clear api calls

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ var bodyParser = require('body-parser');
 var url = 'mongodb://ccms:ccms.15@ds043022.mongolab.com:43022/heroku_app37228477';
 var database;
 
+var endpoints = [
+        "GET /api  List endpoints and recent requests",
+        "POST /api/touch  save the POSTed form data",
+        "GET /api/taskValidationAlwaysSucceed  Always returns HTTP 200 and { 'message': 'success' }",
+        "GET /api/taskValidationAlwaysFail  Always returns HTTP 400 and { 'message': 'fail' }",
+        "GET /api/taskValidationRandomFail  Randomly succeeds roughly 50% of the time it is called. Fails the other 50%"
+    ];
+
 process.on('SIGINT', function () {
     console.log("Closing database connection");
     database.close();
@@ -47,18 +55,56 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 var port = process.env.PORT || 8080;        // set our port
 
-var router = express.Router();              // get an instance of the express Router
-router.all('/', function(req, res) {
+var help = function(req, res) {
     console.log('req ' + req.ref);
     recentRequests(database, function(docs){
-        res.status(200).json({ message: 'success', endpoints: [
-            "GET /api/taskValidationAlwaysSucceed  Always returns HTTP 200 and { 'message': 'success' }",
-            "GET /api/taskValidationAlwaysFail  Always returns HTTP 400 and { 'message': 'fail' }",
-            "GET /api/taskValidationRandomFail  Randomly succeeds roughly 50% of the time it is called. Fails the other 50%"
-        ],
-        recentRequests: docs
+        res.status(200).json({ message: 'success', 
+            endpoints: endpoints,
+            recentRequests: docs || []
         });
     });   
+}
+
+var clear = function(req, res) {
+    console.log('req ' + req.ref);
+    var collection = database.collection('webhook');
+    
+    // Find some documents
+    collection.deleteMany({},function(err) {
+        assert.equal(err, null);
+        console.log("Deleted records");
+
+        res.status(200).json({ message: 'success'});
+
+    }); 
+}
+
+var router = express.Router();              // get an instance of the express Router
+router.get('/', help);
+
+router.post('/touch', function(req, res) {
+    database.collection('webhook').save({
+        "method": req.method,
+        "url": req.originalUrl,
+        "ip": req.ip,
+        "hostname": req.hostname || "",
+        "body": req.body || {}
+    }, function(err, result) {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log('saved to database')
+
+//    MongoClient.connect(url, function(err, db) {
+//      assert.equal(null, err);
+//      insertDocument(db, req, function() {
+//          db.close();
+//      });
+//    });
+
+        res.status(200).json({ message: 'success' });
+    });
 });
 
 router.all('/taskValidation', function(req, res) {
@@ -73,7 +119,9 @@ router.all('/taskValidation', function(req, res) {
         "hostname": req.hostname || "",
         "body": req.body || {}
     }, function(err, result) {
-        if (err) return console.log(err)
+        if (err) {
+            return console.log(err);
+        }
 
         console.log('saved to database')
 
@@ -107,6 +155,8 @@ router.all('/taskValidationAlwaysFail', function(req, res) {
 });
 
 app.use('/api', router);
+app.all('/', help);
+app.all('/clear', clear);
 
 // START THE SERVER
 app.listen(port);
