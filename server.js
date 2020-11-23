7ba55fab9fdde4c6e65f8f8fcc300d251863a467
Harry Moore <harrymoore@yahoo.com>
2020-11-23 10:20:47 -0500

    refactor to no longer use a database

--- server.js
+++ server.js
@@ -1,27 +1,19 @@
 // server.js
-var assert = require('assert');
-var express = require('express'); // call express
-var app = express(); // define our app using express
-var cors = require('cors')
+const express = require('express'); // call express
+const app = express(); // define our app using express
+
+const port = process.env.PORT || 8080; // set our port
+
+const cors = require('cors');
 app.use(cors());
-var Db = require('mongodb').Db;
-var MongoClient = require('mongodb').MongoClient;
-var Server = require('mongodb').Server;
-var bodyParser = require('body-parser');
 
-// var url = 'mongodb://ccms:ccms.15@ds043022.mongolab.com:43022/heroku_app37228477';
-var default_url = 'mongodb://heroku_app37228477:lr5mei2e13o4cgorhhe2vpj1jp@ds043022.mongolab.com';
-var default_port = 27017;
-var default_name = "heroku_app37228477";
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-var db_url = process.env['MONGODB_URI'];
-var db_name = process.env['MONGO_NAME'] || default_name;
-console.log("db address: " + db_url);
-var database;
-
-var COLLECTION_NAME = "webhook";
-
-var endpoints = [
+const endpoints = [
     "GET /api  List endpoints and recent requests",
     "POST /api/touch  save the POSTed form data",
     "GET /api/touch  save the request data",
@@ -30,74 +22,32 @@ var endpoints = [
     "GET /api/taskValidationRandomFail  Randomly succeeds roughly 50% of the time it is called. Fails the other 50%"
 ];
 
-process.on('SIGINT', function () {
-    console.log("Closing database connection");
-    database.close();
-    process.exit(0);
-});
+const MAX_REQUESTS = 25;
+const requests = [];
+function saveRequest(record) {
+    if (requests.length >= MAX_REQUESTS) {
+        requests.shift()
+    }
 
-// var mongoclient = new MongoClient(new Server(db_url), {native_parser: true});
-MongoClient.connect(db_url, function (err, client) {
-    assert.equal(null, err);
-    console.log("Connected correctly to server.");
-    database = client.db(db_name);
-    console.log(database);
-});
-
-var insertDocument = function (db, record, callback) {
-    db.collection(COLLECTION_NAME).insertOne(record, function (err, result) {
-        assert.equal(err, null);
-        console.log("Inserted a document into the restaurants collection.");
-        callback(result);
-    });
-};
-
-var recentRequests = function (db, callback) {
-    // Get the documents collection
-    var collection = db.collection(COLLECTION_NAME);
-
-    // Find some documents
-    collection.find().sort({
-        timestamp: -1
-    }).limit(10).toArray(function (err, docs) {
-        assert.equal(err, null);
-        console.log("Found the following records");
-        console.log(docs)
-        callback(docs);
-    });
+    requests.push(record);
 }
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-var port = process.env.PORT || 8080; // set our port
 
 var help = function (req, res) {
     console.log('req ' + req.ref);
-    recentRequests(database, function (docs) {
-        res.status(200).json({
-            message: 'success',
-            endpoints: endpoints,
-            recentRequests: docs || []
-        });
+    res.status(200).json({
+        message: 'success',
+        endpoints: endpoints,
+        recentRequests: requests || []
     });
 }
 
 var clear = function (req, res) {
     console.log('req ' + req.ref);
-    var collection = database.collection(COLLECTION_NAME);
 
-    // Find some documents
-    collection.deleteMany({}, function (err) {
-        assert.equal(err, null);
-        console.log("Deleted records");
+    requests.splice(0, requests.length);
 
-        res.status(200).json({
-            message: 'success'
-        });
-
+    res.status(200).json({
+        message: 'success'
     });
 }
 
@@ -106,7 +56,7 @@ router.get('/', help);
 
 router.all('/touch', function (req, res) {
 
-    database.collection(COLLECTION_NAME).save({
+    var record = {
         "timestamp": new Date(),
         "method": req.method,
         "url": req.originalUrl,
@@ -115,35 +65,32 @@ router.all('/touch', function (req, res) {
         "body": req.body || {},
         "params": req.params || {},
         "headers": req.headers || {}
-    }, function (err, result) {
-        if (err) {
-            return console.log(err);
-        }
+    };
 
-        console.log('saved to database');
-        res.status(200).json({
-            message: 'success'
-        });
+    saveRequest(record);
+
+    console.log('saved to requests list');
+
+    res.status(200).json({
+        message: 'success'
     });
 });
 
 router.all('/taskValidation', function (req, res) {
 
-    database.collection(COLLECTION_NAME).save({
+    var record = {
         "method": req.method,
         "url": req.originalUrl,
         "ip": req.ip,
         "hostname": req.hostname || "",
         "body": req.body || {}
-    }, function (err, result) {
-        if (err) {
-            return console.log(err);
-        }
+    };
 
-        console.log('saved to database');
-        res.status(200).json({
-            message: 'success'
-        });
+    saveRequest(record);
+
+    console.log('saved to database');
+    res.status(200).json({
+        message: 'success'
     });
 });
 
